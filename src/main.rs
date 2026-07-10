8ebaed068aec1f81daacf2a7b9390cebf54e9c04
TidalConstructionCo <tidal@lauchmail.de>
2026-07-10 22:50:33 +0200

    Add Initial CLI commands (#1)
    Add initial CLI for help, listings and (unimplemented) production
    output.

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,38 @@
-use colored::Colorize;
-
-use crate::gtc_api::{
-    ExchangeOrder, get_bases, get_crafting_recipes, get_exchange_data, get_exchange_orders,
-    get_materials_data,
-};
+use clap::{Parser, Subcommand};
 mod gtc_api;
-// TODO: clean up everything
-// TODO: add proper item API
-// TODO: add missing fields
+mod listings;
+mod production;
+
+/// Helper tool to query the Galactic Tycoons API. Requires the environment variable GT_API_KEY to contain your API key.
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Print all exchange listings for items you are selling.
+    Listings,
+    /// Print your production surplus available for contracts.
+    Production,
+}
+
 #[tokio::main]
 async fn main() {
-    let api_key = get_gt_api_key().unwrap();
-    // let exchange_orders = get_exchange_orders(&api_key);
-    // TODO: hardcode materials once / add a subcommand to refresh this info
-    //     let materials = get_materials_data(&api_key);
-    //     print_orders(&exchange_rders.await, &materials.await);
-    // print_exchange_data(&api_key, &exchange_orders.await).await;
-    let bases = get_bases(&api_key).await;
-    let (recipes, materials) = get_materials_data(&api_key).await;
-    let crafting_recipes = get_crafting_recipes(&recipes, &materials);
-    for base in &bases {
-        // println!("{:?}", base);
-        base.print_production(&crafting_recipes);
-    }
-}
-async fn print_exchange_data(api_key: &str, exchange_orders: &[ExchangeOrder]) -> () {
-    let material_ids: std::collections::HashSet<_> =
-        exchange_orders.iter().map(|e| e.mat_id).collect();
-    let mut tasks = tokio::task::JoinSet::new();
-    for id in material_ids {
-        let key = api_key.to_owned();
-        tasks.spawn(async move { get_exchange_data(key, id).await });
-    }
-    while let Some(res) = &tasks.join_next().await {
-        if let Ok(Some(info)) = res {
-            println!("======== {} ========", info.mat_name);
-            for order in &info.orders {
-                let output = format!(
-                    "{:.2}: {} ({})",
-                    get_float_price(order.unit_price),
-                    order.qty,
-                    order.c_name
-                );
-                if order.c_name == "Tidal Construction Co." {
-                    println!("{}", output.purple());
-                } else {
-                    println!("{output}");
-                }
-            }
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Listings) => {
+            listings::print_listings().await;
+        }
+        Some(Commands::Production) => {
+            production::print_production().await;
+        }
+        None => {
+            println!(
+                "Running gtc without a command is not supported. Run 'gtc help' or 'gtc --help' to see available commands."
+            )
         }
     }
-}
-
-fn get_float_price(price: i32) -> f32 {
-    price as f32 / 100f32
-}
-
-// fn print_orders(orders: &[ExchangeOrder], materials: &[Material]) {
-//     for order in orders.iter() {
-//         let material_info = materials
-//             .iter()
-//             .find(|material| material.id == order.matId)
-//             .unwrap();
-//
-//         println!(
-//             "item: {}, amount: {}, price: {}",
-//             material_info.name, order.qty, order.unitPrice
-//         );
-//     }
-// }
-
-fn get_gt_api_key() -> Option<String> {
-    let api_key = std::env::vars().find(|(key, _value)| key == "GT_API_KEY");
-    if let Some((_, value)) = api_key {
-        return Some(value);
-    }
-    None
 }
